81564d7e021a43f21e3a66448e5707a929ec2f1e
ltaylor2 <ltaylor.ulysses@gmail.com>
2018-05-14 22:18:56 -0230

    starting birding map fixes

--- species_buttons.js
+++
@@ -1,8 +0,0 @@
-var speciesButtons = document.getElementsByClassName("species");
-console.log(speciesButtons.length)
-for (var i = 0; i < speciesButtons.length; i++) {
-  speciesButtons[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-  	alert()
-  });
-}
