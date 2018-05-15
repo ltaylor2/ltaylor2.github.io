1ea354f7f3956980ad8e05c19aea098ffb030e7c
ltaylor2 <ltaylor.ulysses@gmail.com>
2018-05-14 22:09:35 -0230

    init birding map

--- birding_list.js
+++ birding_list.js
@@ -28,33 +28,74 @@ $.get("https://raw.githubusercontent.com/ltaylor2/ltaylor2.github.io/master/Medi
 
 	var species = {};
 	var counts = {};
+	var locations = {};
 
 	// pass through list to isolate all individual species
-	for (var i=0; i<birdArray_raw.length; i++) {
+	for (var i = 0; i < birdArray_raw.length; i++) {
 		var obs = birdArray_raw[i];
 		var common = obs[colNames["common"]];
 		var scientific = obs[colNames["scientific"]];
 		var count = obs[colNames["count"]];
+		var location = [obs[colNames["latitude"]], obs[colNames["longitude"]]]
 		if (count == "X") { count = 1; }
 		else { count = parseInt(count); }
 
 		if (!(common in species)) {
 			species[common] = scientific;
 			counts[common] = count;
+			locations[common] = new Array();
+			locations[common].push(location);
 		} else {
 			counts[common] += count;
+			locations[common].push(location);
 		}
 	}
 
 	var table = "<table><tbody>";
 	for (var key in species) {
 		table += "<tr>";
-		table += "<td>" + key + "</td>";
+		table += "<td><button class=\"species\">" + key + "</button></td>";
 		table += "<td><em>" + species[key] + "</em></td>";
 		table += "<td>" + counts[key] + "</td>";
-		table += "</tr>";
+		table += "</td>";
 	}
 	table += "</tbody></table>";
 
 	$("#eBird-data").append(table);
+	var speciesButtons = document.getElementsByClassName("species");
+	for (var i = 0; i < speciesButtons.length; i++) {
+	  speciesButtons[i].addEventListener("click", function() {
+	    this.classList.toggle("active");
+	  	map.removeLayer(heatMapLayer);
+	  	var common = this.innerText;
+	  	var latlons = locations[common];
+  		var heatData = new ol.source.Vector();
+
+  		var centerCoord = []
+	  	for (var l = 0; l < latlons.length; l++) {
+	  		var lon = parseFloat(latlons[l][0]);
+	  		var lat = parseFloat(latlons[l][1]);
+	  		if (isNaN(lon) || isNaN(lat)) { continue; }
+	  		
+	  		console.log([lon, lat]);
+	  		var coord = ol.proj.fromLonLat([lat, lon]);
+	  		centerCoord = coord;
+	  		var point = new ol.geom.Point(coord);
+	  		var pointFeature = new ol.Feature({
+	  			geometry: point,
+	  			weight: 1,
+	  		});
+	  		heatData.addFeature(pointFeature);
+	  	}
+
+	  	heatMapLayer = new ol.layer.Heatmap({
+	  		source: heatData,
+	  		opacity: 0.5,
+	  	});
+
+	  	map.addLayer(heatMapLayer);
+	  	console.log(centerCoord);
+	  	map.getView().centerOn(centerCoord, [1,1], [0,0]);
+	  });
+	}
 });
