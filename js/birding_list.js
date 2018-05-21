509408aae36d47de16f65250aaf34d99fb13e4bd
ltaylor2 <ltaylor.ulysses@gmail.com>
2018-05-21 00:25:44 -0230

    precalc eBird json data

--- js/birding_list.js
+++ js/birding_list.js
@@ -1,9 +1,7 @@
-$.get("https://raw.githubusercontent.com/ltaylor2/ltaylor2.github.io/master/Media/eBird/MyEBirdData.csv", function(data) {
-	var birdArray_raw = new Array();
-	var rows = data.split("\n");
-	for (var r=1; r<rows.length; r++) {
-		birdArray_raw[r-1] = rows[r].split(",");
-	}
+$.get("https://raw.githubusercontent.com/ltaylor2/ltaylor2.github.io/master/Media/eBird/MyEBirdData.csv", function(data_obs) {
+$.get("https://raw.githubusercontent.com/ltaylor2/ltaylor2.github.io/master/Media/eBird/eBird_orderfamily.csv", function(data_of) {
+	var rows = data_obs.split("\n");
+	
 	var colNames = {"id":0, 
 					"common":1, 
 					"scientific":2,
@@ -29,68 +27,101 @@ $.get("https://raw.githubusercontent.com/ltaylor2/ltaylor2.github.io/master/Medi
 	var species = {};
 	var counts = {};
 	var locations = {};
-
-	// pass through list to isolate all individual species
-	for (var i = 0; i < birdArray_raw.length; i++) {
-		var obs = birdArray_raw[i];
-		var common = obs[colNames["common"]];
-		var scientific = obs[colNames["scientific"]];
-		var count = obs[colNames["count"]];
-		var location = [obs[colNames["latitude"]], obs[colNames["longitude"]]]
+	for (var r=1; r<rows.length; r++) {
+		row = rows[r].split(",");
+		var common = row[colNames["common"]];
+		var scientific = row[colNames["scientific"]];
+		var count = row[colNames["count"]];
+		var location = [row[colNames["latitude"]], row[colNames["longitude"]]]
 		if (count == "X") { count = 1; }
 		else { count = parseInt(count); }
 
 		if (!(common in species)) {
 			species[common] = scientific;
 			counts[common] = count;
-			locations[common] = new Array();
+			locations[common] = [];
 			locations[common].push(location);
+
 		} else {
 			counts[common] += count;
 			locations[common].push(location);
 		}
 	}
 
-	var list = "<ul id=\"bird-list\">";
-	for (var key in species) {
-		list += "<li><button class=\"species\">" + key + "</button></li>";
+
+	birdList = "<div class=\"order-list\">";
+	for (order in orderFamilies) {
+		birdList += "<button class=\"order-header\">" + order + "</button>";
+		birdList += "<div class=\"family-list\">";
+		for (family in orderFamilies[order]) {
+			birdList += "<button class=\"family-header\">" + orderFamilies[order][family] + "</button>";
+
+
+		}
+		birdList += "</div>";
 	}
-	list += "</ul>";
 
-	$("#eBird-data").append(list);
+	birdList += "</div>";
 
-	var speciesButtons = document.getElementsByClassName("species");
-	for (var i = 0; i < speciesButtons.length; i++) {
-	  speciesButtons[i].addEventListener("click", function() {
-	    this.classList.toggle("active");
-	  	map.removeLayer(heatMapLayer)
-	  	var common = this.innerText;
-	  	var latlons = locations[common];
-  		var heatData = new ol.source.Vector();
+	$("#eBird-data").append(birdList);
 
-  		var centerCoord = ol.proj.fromLonLat([-76.3, 38]);
-	  	for (var l = 0; l < latlons.length; l++) {
-	  		var lat = parseFloat(latlons[l][0]);
-	  		var lon = parseFloat(latlons[l][1]);
-	  		if (isNaN(lon) || isNaN(lat)) { continue; }
-	  		
-	  		var coord = ol.proj.fromLonLat([lon, lat]);
-	  		centerCoord = coord;
-	  		var point = new ol.geom.Point(coord);
-	  		var pointFeature = new ol.Feature({
-	  			geometry: point,
-	  			weight: 1,
-	  		});
-	  		heatData.addFeature(pointFeature);
-	  	}
-
-	  	heatMapLayer = new ol.layer.Heatmap({
-	  		source: heatData,
-	  		opacity: 0.5,
-	  	});
-
-	  	map.addLayer(heatMapLayer);
-	  	map.getView().centerOn(centerCoord, [1,1], [0,0]);
-	});
+	var acc = document.getElementsByClassName("order-header");
+	for (var i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener("click", function() {
+	  	this.classList.toggle("active");
+	  	this.classList.add("order-header-active");
+	    var panel = this.nextElementSibling;
+	    if (panel.style.maxHeight){
+	    	this.classList.remove("order-header-active");
+	    	panel.style.maxHeight = null;
+	    } else {
+	      	panel.style.maxHeight = panel.scrollHeight + "px";
+	    }
+	  });
 	}
+})
+})
 });
+
+	// var list = "<ul id=\"bird-list\">";
+	// for (var key in species) {
+	// 	list += "<li><button class=\"species\">" + key + "</button></li>";
+	// }
+	// list += "</ul>";
+
+	// $("#eBird-data").append(list);
+
+	// var speciesButtons = document.getElementsByClassName("species");
+	// for (var i = 0; i < speciesButtons.length; i++) {
+	//   speciesButtons[i].addEventListener("click", function() {
+	//     this.classList.toggle("active");
+	//   	map.removeLayer(heatMapLayer)
+	//   	var common = this.innerText;
+	//   	var latlons = locations[common];
+ //  		var heatData = new ol.source.Vector();
+
+ //  		var centerCoord = ol.proj.fromLonLat([-76.3, 38]);
+	//   	for (var l = 0; l < latlons.length; l++) {
+	//   		var lat = parseFloat(latlons[l][0]);
+	//   		var lon = parseFloat(latlons[l][1]);
+	//   		if (isNaN(lon) || isNaN(lat)) { continue; }
+	  		
+	//   		var coord = ol.proj.fromLonLat([lon, lat]);
+	//   		centerCoord = coord;
+	//   		var point = new ol.geom.Point(coord);
+	//   		var pointFeature = new ol.Feature({
+	//   			geometry: point,
+	//   			weight: 1,
+	//   		});
+	//   		heatData.addFeature(pointFeature);
+	//   	}
+
+	//   	heatMapLayer = new ol.layer.Heatmap({
+	//   		source: heatData,
+	//   		opacity: 0.5,
+	//   	});
+
+	//   	map.addLayer(heatMapLayer);
+	//   	map.getView().centerOn(centerCoord, [1,1], [0,0]);
+	// });
+	// }
