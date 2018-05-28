797f77314da831f38477cf21f4a95a7181cfe0f9
ltaylor2 <ltaylor.ulysses@gmail.com>
2018-05-28 20:15:57 -0230

    finished papers

--- js/papers.js
+++ js/papers.js
@@ -4,11 +4,11 @@ function(papersListRaw)
 	var papersList = papersListRaw.split("\n");
 
 	for (var i = papersList.length; i > 0; i--) {
-		let citation = papersList[i];
-		if (paper != "") {
-			let p = document.createElement("p"):
+		let paper = papersList[i];
+		if (paper && paper != "") {
+			let p = document.createElement("p");
 			p.classList.add("citation");
-			p.innerText = citation;
+			p.innerHTML = paper;
 			$("#papers").append(p);
 		}
 	}
