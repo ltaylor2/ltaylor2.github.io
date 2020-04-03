c31b2a680b4d72ba2d543e3ddd5225f77d10ee48
unknown <ltaylor.ulysses@gmail.com>
2020-04-03 14:16:55 -0400

    first test automatic paper pdf links

--- js/papers.js
+++ js/papers.js
@@ -1,11 +1,41 @@
 $.get("https://raw.githubusercontent.com/ltaylor2/ltaylor2.github.io/master/Media/papers_list.txt",
 function(papersListRaw) 
 {
-	
+
+	var paperFiles = fs.readdirSync("./Media/Papers/");
+
+	findPaperLink = function(author, year) {
+
+		var firstMatches = [];
+		for (var i=0; i<paperFiles.length; i++) {
+			if (paperFiles[i].match(author))
+				firstMatches.push(i);
+		}	
+
+		var jointMatches = [];
+		for (var j=0; j<firstMatches.length; j++) {
+			if (paperFiles[firstMatches[j]].match(year)) {
+				jointMatches.push(firstMatches[j]);
+			}
+		}
+
+		paperFile = paperFiles[jointMatches[0]];
+
+		return "./Media/Papers/" + paperFile;
+	}
+
 	arrangeLink = function(citation) {
 		let firstAuthor = citation.split(',')[0];
-		let year = citation.match(/^\d+|\d+\b|\d+(?=\w)/g)[1];
+		let year = citation.match(/^\d+|\d+\b|\d+(?=\w)/g)[0];
 
+		filePath = findPaperLink(firstAuthor, year);
+
+		let a = document.createElement("a");
+		a.href = filePath;
+		a.classList.add("citation");
+		a.innerHTML = citation;
+
+		return a;
 	}
 
 	var papersList = papersListRaw.split("\n");
@@ -13,9 +43,10 @@ function(papersListRaw)
 	for (var i = papersList.length; i > 0; i--) {
 		let paper = papersList[i];
 		if (paper && paper != "") {
-			let p = document.createElement("p");
-			p.classList.add("citation");
-			p.innerHTML = paper;
+			p = arrangeLink(paper);
+			// let p = document.createElement("p");
+			// p.classList.add("citation");
+			// p.innerHTML = paper;
 			$("#papers").append(p);
 			$("#papers").css("background", "white");
 		}
